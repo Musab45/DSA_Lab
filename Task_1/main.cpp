dc3f9a17b3485ddeb68da3720d30bdc1d5612782
Musab <67883529+Musab45@users.noreply.github.com>
2024-09-15 11:09:21 +0500

    Updated methods

--- Task_1/main.cpp
+++ Task_1/main.cpp
@@ -1,6 +1,5 @@
 #include<iostream>
 #include<string>
-#include<cstdlib>
 
 class House
 {
@@ -10,114 +9,119 @@ private:
 	int bedrooms;
 	float price;
 public:
-	House() {}
+	//Constructor
+	House(std::string ownerName, std::string houseAddress, int numBedrooms, float housePrice)
+		: owner(ownerName), address(houseAddress), bedrooms(numBedrooms), price(housePrice) {}
 
-	//setters
-	void setOwner(std::string owner)
-	{
-		this->owner = owner;
-	}
-	void setAddress(std::string address)
-	{
-		this->address = address;
-	}
-	void setBedrooms(int num)
-	{
-		this->bedrooms = num;
-	}
-	void setPrice(float num)
-	{
-		this->price = num;
-	}
+    House() : owner("Unknown"), address("Unknown"), bedrooms(0), price(0.0f) {}
 
-	//getters
-	std::string getOwner()
-	{
-		return this->owner;
-	}
-	std::string getAddress()
-	{
-		return this->address;
-	}
-	int getBedrooms()
-	{
-		return this->bedrooms;
-	}
-	float getPrice()
-	{
-		return this->price;
-	}
+    //Getters
+    std::string getOwner() 
+    {
+        return owner;
+    }
+    std::string getAddress() 
+    {
+        return address;
+    }
+    int getBedrooms()
+    {
+        return bedrooms;
+    }
+    float getPrice() 
+    {
+        return price;
+    }
+
+    //Setters
+    void setOwner(std::string ownerName) 
+    {
+        owner = ownerName;
+    }
+    void setAddress(std::string houseAddress) 
+    {
+        address = houseAddress;
+    }
+    void setBedrooms(int numBedrooms) 
+    {
+        if (numBedrooms > 2) 
+        {
+            bedrooms = numBedrooms;
+        }
+        else 
+        {
+            std::cout << "Invalid number of bedrooms! Cannot be less than 2" << std::endl;
+        }
+    }
+    void setPrice(float housePrice) 
+    {
+        if (housePrice > 1000) 
+        {
+            price = housePrice;
+        }
+        else 
+        {
+            std::cout << "Invalid price! Cannot be less than 1000" << std::endl;
+        }
+    }
+
+    void displayHouseDetails() {
+        std::cout << "Owner: " << owner << std::endl;
+        std::cout << "Address: " << address << std::endl;
+        std::cout << "Bedrooms: " << bedrooms << std::endl;
+        std::cout << "Price: $" << price << std::endl;
+    }
 };
-
-//Array of available houses
-House available[100];
-
-int houses_in_database = 0;
-
-
-void addHouse(int index)
-{
-	std::string owner;
-	std::string address;
-	int bedrooms = 0;
-	float price = 0.0;
-
-	std::cin.ignore();
-	std::cout << "Enter owner's name: ";
-	getline(std::cin, owner);
-	std::cout << "Enter address: ";
-	getline(std::cin, address);
-	std::cout << "Enter number of bedrooms:";
-	std::cin >> bedrooms;
-	std::cout << "Enter price:";
-	std::cin >> price;
-
-	available[index].setOwner(owner);
-	available[index].setAddress(address);
-	available[index].setBedrooms(bedrooms);
-	available[index].setPrice(price);
-	houses_in_database++;
-}
-
-void showHouse()
-{
-	std::cout << "Owner\t" << "Address\t" << "Bedrooms\t" << "Price\t" << std::endl;
-	for (int i = 0; i <= houses_in_database; i++)
-	{
-		std::cout << available[i].getOwner() << "\t" << available[i].getAddress() << "\t" << available[i].getBedrooms() << "\t" << available[i].getPrice() << "\t" << std::endl;
-	}
-}
-
-int property_add_index = 0;
 
 int main()
 {
-	char main_menu_choice;
+    House* houses = new House[100];
+    std::string owner;
+    std::string address;
+    int bedrooms;
+    float price;
+    int index = 0;
+    char menu_selection;
+    do
+    {
+        std::cout << "   -- Property Manager --   " << std::endl;
+        std::cout << "1. Add Property" << std::endl;
+        std::cout << "2. View Property" << std::endl;
+        std::cout << "3. Exit" << std::endl;
+        std::cout << "Choice: ";
+        std::cin >> menu_selection;
+        system("cls");
+        switch (menu_selection)
+        {
+        case'1':
+            
+            std::cout << "Enter details for property " << index + 1 << std::endl;
+            std::cout << "Owner: ";
+            std::cin >> owner;
+            std::cout << "Address: ";
+            std::cin >> address;
+            std::cout << "Bedrooms: ";
+            std::cin >> bedrooms;
+            std::cout << "Price: ";
+            std::cin >> price;
 
-	do
-	{
-		std::cout << "   -- Property Management Software --   " << std::endl;
-		std::cout << "1. Add Property" << std::endl;
-		std::cout << "2. View Properties" << std::endl;
-		std::cout << "3. Exit" << std::endl;
-		std::cout << "Choice: ";
-
-		std::cin >> main_menu_choice;
-
-		system("cls");
-		switch (main_menu_choice)
-		{
-		case'1':
-			addHouse(property_add_index);
-			system("cls");
-			break;
-		case'2':
-			showHouse();
-			system("pause");
-			system("cls");
-			break;
-		}
-	} while (main_menu_choice != 3);
-
+            // Set the values for each house object
+            houses[index].setOwner(owner);
+            houses[index].setAddress(address);
+            houses[index].setBedrooms(bedrooms);
+            houses[index].setPrice(price);
+            index++;
+            system("cls");
+            break;
+        case'2':
+            for (int i = 0; i < index; i++) {
+                std::cout << "\nDetails of house " << i + 1 << ":" << std::endl;
+                houses[i].displayHouseDetails();
+            }
+            system("pause");
+            system("cls");
+            break;
+        }
+    } while (menu_selection != 3);
 	return 0;
 }
