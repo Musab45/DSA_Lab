4c3edf51db1680c610955748888dce2d21b13e03
Musab <67883529+Musab45@users.noreply.github.com>
2024-09-15 11:54:42 +0500

    Search by city filter applied

--- Task_1/main.cpp
+++ Task_1/main.cpp
@@ -5,24 +5,24 @@ class House
 {
 private:
 	std::string owner;
-	std::string address;
+	std::string city;
 	int bedrooms;
 	float price;
 public:
 	//Constructor
-	House(std::string ownerName, std::string houseAddress, int numBedrooms, float housePrice)
-		: owner(ownerName), address(houseAddress), bedrooms(numBedrooms), price(housePrice) {}
+	House(std::string ownerName, std::string housecity, int numBedrooms, float housePrice)
+		: owner(ownerName), city(housecity), bedrooms(numBedrooms), price(housePrice) {}
 
-    House() : owner("Unknown"), address("Unknown"), bedrooms(0), price(0.0f) {}
+    House() : owner("Unknown"), city("Unknown"), bedrooms(0), price(0.0f) {}
 
     //Getters
     std::string getOwner() 
     {
         return owner;
     }
-    std::string getAddress() 
+    std::string getcity() 
     {
-        return address;
+        return city;
     }
     int getBedrooms()
     {
@@ -38,9 +38,9 @@ public:
     {
         owner = ownerName;
     }
-    void setAddress(std::string houseAddress) 
+    void setcity(std::string housecity) 
     {
-        address = houseAddress;
+        city = housecity;
     }
     void setBedrooms(int numBedrooms) 
     {
@@ -68,7 +68,7 @@ public:
     void displayHouseDetails() 
     {
         std::cout << "Owner: " << owner << std::endl;
-        std::cout << "Address: " << address << std::endl;
+        std::cout << "City: " << city << std::endl;
         std::cout << "Bedrooms: " << bedrooms << std::endl;
         std::cout << "Price: $" << price << std::endl;
     }
@@ -78,13 +78,14 @@ int main()
 {
     House* houses = new House[100];
     std::string owner;
-    std::string address;
+    std::string city;
     int bedrooms;
     int bedrooms_check;
     float price;
     float price_check;
     int index = 0;
     char menu_selection;
+    char filter_selection;
     do
     {
         std::cout << "   -- Property Manager --   " << std::endl;
@@ -103,8 +104,8 @@ int main()
             std::cin.ignore();
             std::cout << "Owner: ";
             std::getline(std::cin, owner);
-            std::cout << "Address: ";
-            std::getline(std::cin, address);
+            std::cout << "City: ";
+            std::getline(std::cin, city);
             std::cout << "Bedrooms: ";
             std::cin >> bedrooms_check;
             std::cout << "Price: ";
@@ -117,7 +118,7 @@ int main()
 
                 //values for each house object
                 houses[index].setOwner(owner);
-                houses[index].setAddress(address);
+                houses[index].setcity(city);
                 houses[index].setBedrooms(bedrooms);
                 houses[index].setPrice(price);
                 index++;
@@ -132,14 +133,40 @@ int main()
             system("cls");
             break;
         case'2':
-            for (int i = 0; i < index; i++) 
+            std::cout << "1. View all properties" << std::endl;
+            std::cout << "2. Filter by city" << std::endl;
+            std::cout << "3. Filter by price" << std::endl;
+            std::cout << "4. Filter by number of bedrooms" << std::endl;
+            std::cout << "Choice: ";
+            std::cin >> filter_selection;
+            system("cls");
+            switch (filter_selection)
             {
-                std::cout << "\nDetails of house " << i + 1 << ":" << std::endl;
-                houses[i].displayHouseDetails();
+            case'1':
+                for (int i = 0; i < index; i++)
+                {
+                    std::cout << "\nDetails of house " << i + 1 << ":" << std::endl;
+                    houses[i].displayHouseDetails();
+                }
+                break;
+            case'2':
+                std::cout << "Enter city name: ";
+                std::string filter_city;
+                std::cin.ignore();
+                std::getline(std::cin, filter_city);
+
+                for (int i = 0; i < index; i++)
+                {
+                    if (houses[i].getcity().find(filter_city) != std::string::npos)
+                    {
+                        std::cout << "\nDetails of house " << i + 1 << ":" << std::endl;
+                        houses[i].displayHouseDetails();
+                    }
+                }
+                break;
             }
             system("pause");
             system("cls");
-            break;
         }
     } while (menu_selection != 3);
 	return 0;
